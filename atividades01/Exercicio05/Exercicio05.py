fee863becac74b4e5be12103cb8130d4345adeda
Fernando Chiareli Ferreira <fernando.ferreira.chiareli@gmail.com>
2020-08-18 19:00:30 -0300

    ex5

---
+++ atividades01/Exercicio05/Exercicio05.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+var = int(input('digite um número: '))
+#dobro
+print('O dobro de {} vale {}'.format(var, var*2))
+print('O triplo de {} vale {}'.format(var, var*3))
+print('A raiz quadrada de {} vale {:.2f}'.format(var, sqrt(var)))
